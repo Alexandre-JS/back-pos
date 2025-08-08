48db54cc2589e658f7834e05179c41e45851c90e
Alexandre Sitole <alexandresitole@gmail.com>
2025-08-08 18:03:01 +0200

    chore: update environment variable formatting and refactor data source initialization

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,10 +1,9 @@
-import { Connection, createConnection } from 'typeorm';
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
 import { typeOrmConfig } from '../config/typeorm.config';
 
-export const createAppDataSource = async (): Promise<Connection> => {
-  return await createConnection({
-    ...typeOrmConfig,
-    synchronize: false, // Nunca usar migrations com synchronize: true
-    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-  });
-};
+export const AppDataSource = new DataSource({
+  ...typeOrmConfig,
+  synchronize: false,
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+});
